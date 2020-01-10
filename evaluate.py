7bc76d485800712f6ce379b99d35dd19d00bcdcd
Chris Seals <seals_christopher@bah.com>
2020-01-10 11:54:12 -0600

    test with regressors, features

--- evaluate.py
+++ evaluate.py
@@ -1,18 +1,17 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.metrics import (
-    accuracy_score,
-    classification_report,
-    cohen_kappa_score,
-    make_scorer,
-)
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
+from sklearn.metrics import accuracy_score, cohen_kappa_score, make_scorer
 from sklearn.preprocessing import RobustScaler
-import matplotlib.pyplot as plt
-from mlxtend.plotting import plot_learning_curves
+
 import mlflow, mlflow.sklearn
 from collections import Counter
 import joblib
+
+#cols_to_drop = ["game_session", "installation_id", "accuracy_group"]
+cols_to_drop = ['accuracy_group']
+
+features = joblib.load("features.pkl")
 
 
 def quick_eval(train, estimator, scale=False, cv=False, pc=False):
@@ -25,10 +24,8 @@ def quick_eval(train, estimator, scale=False, cv=False, pc=False):
         from datetime import datetime
 
         start = datetime.now()
-        #X = train.drop("accuracy_group", axis=1)._get_numeric_data()
-        cols_to_drop = ['game_session', 'installation_id', 'accuracy_group']
-        features = joblib.load('features.pkl')
-        X = train.drop(cols_to_drop, axis=1)[features]
+        features = joblib.load("features.pkl")
+        X = train.drop(cols_to_drop, axis=1)
         y = train.accuracy_group
 
         if scale:
@@ -46,19 +43,13 @@ def quick_eval(train, estimator, scale=False, cv=False, pc=False):
         runtime = datetime.now() - start
         accuracy = accuracy_score(y_test, y_pred)
         qwk = cohen_kappa_score(y_test, y_pred, weights="quadratic")
-        #report = classification_report(y_test, y_pred, output_dict=True)
-        mlflow.log_metric("Runtime min", runtime.seconds/60)
+        # report = classification_report(y_test, y_pred, output_dict=True)
+        mlflow.log_metric("Runtime min", runtime.seconds / 60)
         mlflow.log_param("features_shape", X.shape)
         mlflow.log_param("estimator", estimator_name)
         mlflow.log_metric("Accuracy", accuracy)
         mlflow.log_metric("QWK", qwk)
         mlflow.sklearn.log_model(estimator, "model")
-
-        #classes = [str(x) for x in [0, 1, 2, 3]]
-
-        # create a metric for each piece of the confusion matrix
-        #for i in classes:
-        #    [mlflow.log_metric(f"{i}-{k}", v) for k, v in report[i].items()]
 
         if cv:
             cv_score = cross_val_score(
@@ -72,7 +63,7 @@ def quick_eval(train, estimator, scale=False, cv=False, pc=False):
         else:
             print(f"The accuracy of {estimator_name} is {accuracy}")
             print(f"The QWK of {estimator_name} is {qwk}")
-            #print(report)
+            # print(report)
 
             return estimator
 
@@ -82,7 +73,7 @@ def get_class_pred(pred, train_t):
     """
     Fast cappa eval function for lgb.
     """
-    dist = Counter(train_t['accuracy_group'])
+    dist = Counter(train_t["accuracy_group"])
     for k in dist:
         dist[k] /= len(train_t)
 
@@ -111,7 +102,7 @@ def get_class_pred_cjs(pred, train_t):
     """
     Fast cappa eval function for lgb.
     """
-    dist = Counter(train_t['accuracy_group'])
+    dist = Counter(train_t["accuracy_group"])
     for k in dist:
         dist[k] /= len(train_t)
 
@@ -122,11 +113,11 @@ def get_class_pred_cjs(pred, train_t):
         bound[i] = np.percentile(pred, acum * 100)
 
     def classify(x):
-        if x <= (bound[0]*1.1):
+        if x <= (bound[0] * 1.1):
             return 0
         elif x <= bound[1]:
             return 1
-        elif x <= bound[2]*.9:
+        elif x <= bound[2] * 0.9:
             return 2
         else:
             return 3
@@ -134,3 +125,40 @@ def get_class_pred_cjs(pred, train_t):
     y_pred = np.array(list(map(classify, pred)))
 
     return y_pred
+
+
+def cv_reg(estimator, train_df, n_splits=7):
+    """ Function to run cappa cross val on a given estimator"""
+
+    with mlflow.start_run():
+        results = []
+        kf = KFold(n_splits=n_splits)
+        i = 1
+        for i, (train_idx, test_idx) in enumerate(kf.split(train_df.index)):
+            train, test = train_df.iloc[train_idx], train_df.iloc[test_idx]
+            X_train, y_train = train.drop(cols_to_drop, axis=1), train.accuracy_group
+            X_test, y_test = test.drop(cols_to_drop, axis=1), test.accuracy_group
+            estimator.fit(X_train, y_train)
+            prediction = estimator.predict(X_test)
+            y_pred = get_class_pred(prediction, train_df)
+            qwk = cohen_kappa_score(y_pred, y_test, weights="quadratic")
+            print(f"{i + 1}/{kf.n_splits} Fold start| QWK={qwk}")
+            results.append(qwk)
+
+        mlflow.log_metric("CV qwk", np.mean(results))
+        mlflow.log_metric("QWK", np.max(results))
+        mlflow.log_param("estimator", estimator)
+        mlflow.set_tag("features", "balanced")
+
+    return
+
+
+def make_submission(preds, train_df):
+    preds = get_class_pred(preds, train_df)
+    # assert len(preds)==1000
+    sample = pd.read_csv("data/sample_submission.csv")
+    submission = pd.DataFrame()
+    submission["installation_id"] = sample["installation_id"]
+    submission["accuracy_group"] = preds
+    submission.to_csv("preds.csv", index=False)
+    return submission
